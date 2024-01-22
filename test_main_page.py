1ec3172c7337ed29dc302f014090e88c72ade565
azhiglinskiy <zhiglanton@gmail.com>
2024-01-22 18:09:08 +0300

    added is_element_presented BasePage method

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,15 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from pages.main_page import MainPage
 
 
-def test_placeholder(browser):
-    pass
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
