b5d857c648579cc4ff57671396f330ebe38625d0
azhiglinskiy <zhiglanton@gmail.com>
2024-01-22 17:31:26 +0300

    PageObject init

--- test_items.py
+++ test_items.py
@@ -3,12 +3,5 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-TIMEOUT = 10
-
-
-def test_check_add_to_basket_form_presence(browser):
-    url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    browser.get(url)
-
-    add_to_basket_button = WebDriverWait(browser, TIMEOUT).until(EC.presence_of_element_located((By.ID,
-                                                                                                 "add_to_basket_form")))
+def test_placeholder(browser):
+    pass
