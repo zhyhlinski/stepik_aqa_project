0f41cb07e700510e05ac450b154d502f6f14e41b
azhiglinskiy <zhiglanton@gmail.com>
2024-01-22 18:55:07 +0300

    added login_page check

---
+++ pages/login_page.py
@@ -0,0 +1,19 @@
+from pages.base_page import BasePage
+from pages.locators import LoginPageLocators
+from selenium import webdriver
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM)
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM)
